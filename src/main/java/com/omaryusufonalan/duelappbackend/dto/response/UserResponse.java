888491ef9df6bd4829ecea9c6c12eff680dced6c
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-04-24 21:24:31 +0300

    Create UserResponse.java

---
+++ src/main/java/com/omaryusufonalan/duelappbackend/dto/response/UserResponse.java
@@ -0,0 +1,19 @@
+package com.omaryusufonalan.duelappbackend.dto.response;
+
+import com.omaryusufonalan.duelappbackend.core.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserResponse {
+    private Long id;
+
+    private String username;
+
+    private Role role;
+}
