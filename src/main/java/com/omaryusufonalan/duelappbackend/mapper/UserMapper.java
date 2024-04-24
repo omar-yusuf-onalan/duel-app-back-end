a4d474e1cb4d8fa9598aeef2dc4ac9fffb445dd9
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-04-24 21:26:52 +0300

    Create UserMapper.java

---
+++ src/main/java/com/omaryusufonalan/duelappbackend/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.omaryusufonalan.duelappbackend.mapper;
+
+import com.omaryusufonalan.duelappbackend.core.BaseMapper;
+import com.omaryusufonalan.duelappbackend.dto.request.UserRequest;
+import com.omaryusufonalan.duelappbackend.dto.response.UserResponse;
+import com.omaryusufonalan.duelappbackend.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User, UserRequest, UserResponse> {
+}
