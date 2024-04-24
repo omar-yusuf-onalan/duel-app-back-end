344ff5584822898c08846144c9ede9ee00fdce04
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-04-24 21:26:50 +0300

    Create BaseMapper.java

---
+++ src/main/java/com/omaryusufonalan/duelappbackend/core/BaseMapper.java
@@ -0,0 +1,25 @@
+package com.omaryusufonalan.duelappbackend.core;
+
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
+
+import java.util.List;
+
+public interface BaseMapper<Entity extends BaseEntity, Request, Response> {
+    @Named("requestToEntity")
+    Entity requestToEntity(Request request);
+
+    @Named("responseToEntity")
+    Entity responseToEntity(Response response);
+
+    @Named("entityToRequest")
+    Request entityToRequest(Entity entity);
+
+    @Named("entityToResponse")
+    Response entityToResponse(Entity entity);
+
+    @Named("entityToListResponse")
+    List<Response> entityToListResponse(List<Entity> entity);
+
+    void update(@MappingTarget Entity targetEntity, Request updatingRequest);
+}
