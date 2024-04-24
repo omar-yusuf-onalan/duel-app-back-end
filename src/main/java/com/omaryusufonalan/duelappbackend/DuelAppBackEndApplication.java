2428f8f1702ae76e650826eb9fab18232abe8c8f
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-04-24 20:47:56 +0300

    Create workspace

---
+++ src/main/java/com/omaryusufonalan/duelappbackend/DuelAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.omaryusufonalan.duelappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuelAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuelAppBackEndApplication.class, args);
+	}
+
+}
