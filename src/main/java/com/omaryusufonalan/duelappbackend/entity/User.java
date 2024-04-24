305bfb26a32ac9062879cd1a223bb4261e44a233
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-04-24 21:13:54 +0300

    Implement UserDetails

--- src/main/java/com/omaryusufonalan/duelappbackend/entity/User.java
+++ src/main/java/com/omaryusufonalan/duelappbackend/entity/User.java
@@ -7,6 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "_user")
@@ -14,7 +20,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
     @Column(nullable = false, updatable = false)
     private String username;
 
@@ -27,4 +33,29 @@ public class User extends BaseEntity {
 
     @Column(nullable = false)
     private String profilePicture;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
