19b46c297df31c42b1c0c35733278accd7c4e26e
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-04-24 21:12:58 +0300

    Create AuthenticationResponse.java

---
+++ src/main/java/com/omaryusufonalan/duelappbackend/dto/response/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.omaryusufonalan.duelappbackend.dto.response;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AuthenticationResponse {
+    private String token;
+}
+
