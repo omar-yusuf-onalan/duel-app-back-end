cc2dcf7810a6c668ce0a7bdc1a39b570ba227f22
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-04-29 16:18:57 +0300

    Add cards

--- src/main/java/com/omaryusufonalan/duelappbackend/dto/response/DeckResponse.java
+++ src/main/java/com/omaryusufonalan/duelappbackend/dto/response/DeckResponse.java
@@ -1,10 +1,13 @@
 package com.omaryusufonalan.duelappbackend.dto.response;
 
 import com.omaryusufonalan.duelappbackend.dto.only.OnlyUserDTO;
+import com.omaryusufonalan.duelappbackend.entity.Card;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,8 +15,10 @@ import lombok.Setter;
 @Setter
 public class DeckResponse {
     private Long id;
-
     private String name;
 
     private OnlyUserDTO user;
+    private List<Card> mainDeck;
+    private List<Card> extraDeck;
+    private List<Card> sideDeck;
 }
