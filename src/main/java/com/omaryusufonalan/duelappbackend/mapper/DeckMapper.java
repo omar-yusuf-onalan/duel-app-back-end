c9562d1fe9ea90f65463349902ca6152a6a3a7ec
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-04-29 15:02:35 +0300

    Create DeckMapper.java

---
+++ src/main/java/com/omaryusufonalan/duelappbackend/mapper/DeckMapper.java
@@ -0,0 +1,11 @@
+package com.omaryusufonalan.duelappbackend.mapper;
+
+import com.omaryusufonalan.duelappbackend.core.BaseMapper;
+import com.omaryusufonalan.duelappbackend.dto.request.DeckRequest;
+import com.omaryusufonalan.duelappbackend.dto.response.DeckResponse;
+import com.omaryusufonalan.duelappbackend.entity.Deck;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface DeckMapper extends BaseMapper<Deck, DeckRequest, DeckResponse> {
+}
