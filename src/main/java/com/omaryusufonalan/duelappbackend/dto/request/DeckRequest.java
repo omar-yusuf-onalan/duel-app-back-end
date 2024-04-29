aac7816aae7f121b1e8fdd1ddc5c64febf23b592
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-04-29 16:14:24 +0300

    Add NotBlank annotation

--- src/main/java/com/omaryusufonalan/duelappbackend/dto/request/DeckRequest.java
+++ src/main/java/com/omaryusufonalan/duelappbackend/dto/request/DeckRequest.java
@@ -1,6 +1,7 @@
 package com.omaryusufonalan.duelappbackend.dto.request;
 
 import com.omaryusufonalan.duelappbackend.dto.only.OnlyUserDTO;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DeckRequest {
+    @NotBlank(message = "Deck name cannot be blank!")
     private String name;
 
     private OnlyUserDTO user;
